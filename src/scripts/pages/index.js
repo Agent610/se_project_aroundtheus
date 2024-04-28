7416837ff51af9b8dc55a680934c499dc023876f
Agent610 <BhavsarParthD96@Gmail.com>
2024-04-28 17:57:35 -0400

    Merge branch 'main' of github.com:Agent610/se_project_aroundtheus

--- src/scripts/pages/index.js
+++ src/scripts/pages/index.js
@@ -1,5 +1,5 @@
-import Card from "../components/Card.js";
-import FormValidator from "../components/formvalidator.js";
+import Card from "../../../components/Card.js";
+import FormValidator from "../../../components/formvalidator.js";
 
 const initialCards = [
   {
